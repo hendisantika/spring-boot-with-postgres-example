3eac5fe67530fc3a168dbd8a3573204498df7174
hendisantika <hendisantika@yahoo.co.id>
2024-08-27 07:28:00 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootwithpostgresexample/SpringBootWithPostgresExampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootwithpostgresexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithPostgresExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithPostgresExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootwithpostgresexample/SpringBootWithPostgresExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootwithpostgresexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithPostgresExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
